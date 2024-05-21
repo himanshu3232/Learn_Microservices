fa06e79cea6d47b995efccb68824187c47f77f3a
himanshu3232 <himanshu2000ranjan@gmail.com>
2024-05-21 13:33:43 +0530

    created add customer logic with exception handling

--- accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.easybytes.accounts.controller;
 import com.easybytes.accounts.constants.AccountConstants;
 import com.easybytes.accounts.dto.CustomerDto;
 import com.easybytes.accounts.dto.ResponseDto;
+import com.easybytes.accounts.service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
+    private IAccountsService accountsService;
+
     @PostMapping
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+        accountsService.createAccount(customerDto);
         return ResponseEntity.
                 status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountConstants.STATUS_201,AccountConstants.MESSAGE_201));
--- accounts/src/main/java/com/easybytes/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/easybytes/accounts/exception/GlobalExceptionHandler.java
@@ -4,12 +4,15 @@ import com.easybytes.accounts.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException
             (CustomerAlreadyExistsException exception, WebRequest webRequest){
 
